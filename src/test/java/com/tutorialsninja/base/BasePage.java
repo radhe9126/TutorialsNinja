34b9a822009a01da6d9a47355097aec18c4fc932
Radheshyam <qaengineer9126@gmail.com>
2025-02-18 22:09:40 +0530

    BasePage created

--- src/test/java/com/tutorialsninja/base/BasePage.java
+++ src/test/java/com/tutorialsninja/base/BasePage.java
@@ -1,5 +1,13 @@
 package com.tutorialsninja.base;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+public class BasePage {
+	private WebDriver driver;
+	
+	public BasePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
 }
